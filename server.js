7149e8cda8c4a80db058a7a873c7034c39f2c57d
Philippe Leblond <philippe.leblond@cbd.int>
2013-10-04 16:54:48 -0400

    initial commit of node app

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 var express = require('express');
 var fs = require('fs');
 var http = require('http');
+var https = require('https');
 var httpProxy = require('http-proxy');
+
+var httpsOptions = {
+  key: fs.readFileSync('../certificates/cbd_int.pem'),
+  cert: fs.readFileSync('../certificates/cbd_int.pem')
+};
 
 // Create server
         
 var app = express();
-var server = http.createServer(app);
+var server1 = http.createServer(app);
+var server2 = https.createServer(httpsOptions, app);
 
 // Configure local paths
 
 app.use(express.compress());
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res) {
-	fs.readFile(__dirname + '/app/index.html', 'utf8', function(err, text) {
-    	res.send(text);
-	});
-});
+app.get('/app/*', function(req, res) { res.send('404', 404); } );
+
+app.get('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'bch.cbd.int', port: 80 }); } );
+app.put('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'bch.cbd.int', port: 80 }); } );
+app.post('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'bch.cbd.int', port: 80 }); } );
+app.delete('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'bch.cbd.int', port: 80 }); } );
+
+app.get('/*', function(req, res) { fs.readFile(__dirname + '/app/index.html', 'utf8', function(err, text) { res.send(text); }) });
 
 // Configure proxy
 
 var proxy = new httpProxy.RoutingProxy();
 
-app.use(function (req, res, next) {
-    return proxy.proxyRequest(req, res, { changeOrigin: true, host: 'chm.cbd.int', port: 80 } );
-});
-
 // Start server
-
-server.listen(3000);
+console.log("listening on port 2000/2001")
+server1.listen(2000);
+server2.listen(2001);

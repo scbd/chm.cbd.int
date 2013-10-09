6287c20f183c6b4f4873dcc00525e10927b09a36
Stephane Bilodeau <bilodeaustephane@gmail.com>
2013-10-09 10:33:27 -0400

    Updated karma.conf

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path, that will be used to resolve files and exclude
-    basePath: '',
+    basePath: '..',
 
 
     // frameworks to use
@@ -14,28 +14,37 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '../app/libs/**/jquery-2.0.3.min.js',
-      '../app/libs/**/bootstrap.js',
-      '../app/libs/**/angular.js',
-      '../app/libs/**/*.js',
-      '../app/js/app-chm.js',
-      './adapters/lazy-load-mock.js',
-      '../app/**/*.js',
-      '../app/**/*.spec.js',
-      './**/*.spec.js'
+      'app/libs/**/jquery-2.0.3.min.js',
+      'app/libs/**/bootstrap.js',
+      'app/libs/**/angular.js',
+      'tests/libs/angularjs/1.1.4/*.js',
+      'app/libs/**/*.js',
+      'app/js/app-chm.js',
+      'tests/adapters/lazy-load-mock.js',
+      'app/**/*.js',
+      'app/**/*.partial.html',
+      '**/*.spec.js'
     ],
 
 
     // list of files to exclude
     exclude: [
-      '../app/abs/**/*.js'      
+      'app/abs/**/*.js'
     ],
 
+    preprocessors : {
+      '**/*.partial.html' : ['ng-html2js']
+    },
+
+
+    ngHtml2JsPreprocessor: {
+      moduleName: 'partialTemplateCache',
+      prependPrefix: '/',
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
     reporters: ['progress'],
-
 
     // web server port
     port: 9876,

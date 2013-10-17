3c5e5c6257d937daba9e7f684c5844c0502e2e29
sbilodeau <bilodeaustephane@gmail.com>
2013-10-17 15:20:58 -0400

    Added my-tasks (stub)

---
+++ app/chm/directives/management/my-tasks.js
@@ -0,0 +1,124 @@
+﻿angular.module('kmApp').compileProvider // lazy
+.directive("myTasks", ['authHttp', function ($http) {
+	return {
+		priority: 0,
+		restrict: 'EAC',
+		templateUrl: '/app/chm/directives/management/my-tasks.partial.html',
+		replace: true,
+		transclude: false,
+		scope : true,
+		link : function($scope) {
+			$scope.workflows = null;
+			$scope.load();
+		},
+		controller: [ "$scope", "authHttp", "IStorage", function ($scope, $http, storage) 
+		{
+			//==============================
+			//
+			//==============================
+			$scope.load = function() {
+				$scope.__loading = true;
+				$scope.__error   = null;
+
+				$http.get('/api/v2013/workflows-task/mytasks')
+					.success(function(data) {
+						$scope.workflows = data.workflows;
+						$scope.__loading = false;
+					})
+					.error(function(error){
+						$scope.__error   = error;
+						$scope.__loading = false;
+					});
+			}
+			//==============================
+			//
+			//==============================
+			$scope.edit = function (workflow) {
+				$location.url("/management/edit/" + workflow.info.type + "?uid=" + workflow.info.identifier);
+			};
+		}]
+	}
+}])
+
+//==================================================
+//
+// My Pending Tasks
+//
+//==================================================
+angular.module('kmApp').compileProvider // lazy
+.directive("myPendingTasks", [function () {
+	return {
+		priority: 0,
+		restrict: 'EAC',
+		templateUrl: '/app/chm/directives/management/my-tasks.partial.html',
+		replace: true,
+		transclude: false,
+		scope : true,
+		link : function($scope) {
+			$scope.workflows = {}; // Model
+			$scope.load();
+		},
+		controller: [ "$scope", "authHttp", "IStorage", function ($scope, $http, storage) 
+		{
+			//==============================
+			//
+			//==============================
+			$scope.load = function() {
+				$scope.__loading = true;
+				$scope.__error   = null;
+
+				$http.get('/api/v2013/workflows-mocks/mypendingtasks')
+					.success(function(data) {
+						$scope.workflows = data.workflows;
+						$scope.__loading = false;
+					})
+					.error(function(error){
+						$scope.__error   = error;
+						$scope.__loading = false;
+					});
+			}
+		}]
+	}
+}])
+
+//==================================================
+//
+// My Completed Tasks
+//
+//==================================================
+angular.module('kmApp').compileProvider // lazy
+.directive("myCompletedTasks", [function () {
+	return {
+		priority: 0,
+		restrict: 'EAC',
+		templateUrl: '/app/chm/directives/management/my-tasks.partial.html',
+		replace: true,
+		transclude: false,
+		scope : true,
+		link : function($scope) {
+			$scope.workflows = {}; // Model
+			$scope.load();
+		},
+		controller: [ "$scope", "authHttp", "IStorage", function ($scope, $http, storage) 
+		{
+			//==============================
+			//
+			//==============================
+			$scope.load = function() {
+				$scope.__loading = true;
+				$scope.__error   = null;
+
+				$http.get('/api/v2013/workflows-mocks/mycompletedtasks')
+					.success(function(data) {
+						$scope.workflows = data.workflows;
+						$scope.__loading = false;
+					})
+					.error(function(error){
+						$scope.__error   = error;
+						$scope.__loading = false;
+					});
+			}
+		}]
+	}
+}])
+;

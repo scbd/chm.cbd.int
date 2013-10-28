2ed7acbab8cee5fd8e7eeb50447684c61c7e15aa
Philippe Leblond <philippe.leblond@cbd.int>
2013-10-28 19:53:08 -0400

    bugfixes + moved results to new directives

--- app/chm/directives/search/search.js
+++ app/chm/directives/search/search.js
@@ -14,7 +14,7 @@ angular.module('kmApp').compileProvider // lazy
         },
         link: function ($scope, element, attrs, ngModelController)
         {
-            ngProgress.start();
+            //ngProgress.start();
         },
         controller: ['$scope', '$q', '$timeout', 'ngProgress', function ($scope, $q, $timeout, ngProgress)
         {
@@ -123,7 +123,8 @@ angular.module('kmApp').compileProvider // lazy
 
                 var queryParameters = {
                     'q': q,
-                    'fl': 'id,title_t,description_t,url_ss,schema_EN_t,date_dt,government_EN_t,schema_s',
+                    'sort': 'createdDate_dt desc, title_t asc',
+                    'fl': 'id,title_t,description_t,url_ss,schema_EN_t,date_dt,government_EN_t,schema_s,number_d,aichiTarget_ss,reference_s,sender_s,meeting_ss,recipient_ss,symbol_s,eventCity_EN_t,eventCountry_EN_t,startDate_s,endDate_s,body_s,code_s,meeting_s,group_s',
                     'wt': 'json',
                     'start': $scope.currentPage * $scope.itemsPerPage,
                     'rows': 25,
@@ -150,7 +151,7 @@ angular.module('kmApp').compileProvider // lazy
 
                     $scope.pageCount = Math.ceil(data.response.numFound / $scope.itemsPerPage);
 
-                    ngProgress.complete();
+                    //ngProgress.complete();
 
                     if(!$scope.schemas) {
                         var queryFacetsParameters = {
@@ -159,15 +160,17 @@ angular.module('kmApp').compileProvider // lazy
                             'wt': 'json',
                             'rows': 0,
                             'facet': true,
-                            'facet.field': ['schema_CEN_s', 'government_CEN_s', 'thematicAreas_ss'],
+                            'facet.field': ['schema_REL_ss', 'government_REL_ss', 'aichiTarget_REL_ss', 'thematicArea_REL_ss'],
                             'facet.limit': 256
                         };
 
                         $http.get('/api/v2013/index/select', { params: queryFacetsParameters }).success(function (data) {
 
-                            $scope.schemas = readFacets(data.facet_counts.facet_fields.schema_CEN_s);
-                            $scope.governments = readFacets(data.facet_counts.facet_fields.government_CEN_s);
-                            $scope.thematicAreas = readFacets2(data.facet_counts.facet_fields.thematicAreas_ss);
+                            $scope.schemas = readFacets2(data.facet_counts.facet_fields.schema_REL_ss);
+                            $scope.governments = readFacets2(data.facet_counts.facet_fields.government_REL_ss);
+                            $scope.regions = readFacets2(data.facet_counts.facet_fields.government_REL_ss);
+                            $scope.aichiTargets = readFacets2(data.facet_counts.facet_fields.government_REL_ss);
+                            $scope.thematicAreas = readFacets2(data.facet_counts.facet_fields.thematicArea_REL_ss);
 
                         }).error(function (error) { console.log('onerror'); console.log(error); } );
                     }
@@ -202,8 +205,8 @@ angular.module('kmApp').compileProvider // lazy
 
             $scope.queryScheduled = null;
             function search() {
-                ngProgress.reset();
-                ngProgress.start();
+                //ngProgress.reset();
+                //ngProgress.start();
 
                 if($scope.queryScheduled)
                     $timeout.cancel($scope.queryScheduled);
@@ -340,4 +343,186 @@ angular.module('kmApp').compileProvider // lazy
     }
 })
 
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('rowAichiTarget', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/row-aichi-target.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+        }]
+    };
+});
 
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('rowNationalTarget', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/row-national-target.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+            $scope.mapToNumber = function mapToNumber (code) {
+                return new Number(code.substr(-2));
+            };
+        }]
+    };
+});
+
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('rowProgressAssessment', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/row-progress-assessment.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+        }]
+    };
+});
+
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('rowNotification', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/row-notification.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+            $scope.formatDate = function formatDate (date) {
+                return moment(date).format('MMMM Do YYYY');
+            };
+        }]
+    };
+});
+
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('rowPressRelease', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/row-press-release.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+            $scope.formatDate = function formatDate (date) {
+                return moment(date).format('MMMM Do YYYY');
+            };
+        }]
+    };
+});
+
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('rowMeeting', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/row-meeting.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+            $scope.formatDate = function formatDate (date) {
+                return moment(date).format('MMMM Do YYYY');
+            };
+        }]
+    };
+});
+
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('rowStatement', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/row-statement.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+            $scope.formatDate = function formatDate (date) {
+                return moment(date).format('MMMM Do YYYY');
+            };
+            $scope.cleanTitle = function cleanTitle (text) {
+                //text = text.replace('Braulio F. de Souza Dias', 'Braulio Ferreira de Souza Dias');
+                //text = text.replace('Braulio F. de Souza Dias', 'Braulio Ferreira de Souza Dias');
+                //text = text.replace('Braulio F. de Souza Dias', 'Braulio Ferreira de Souza Dias');
+
+               return text;
+            };
+        }]
+    };
+});
+
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('rowAnnouncement', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/row-announcement.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+            $scope.formatDate = function formatDate (date) {
+                return moment(date).format('MMMM Do YYYY');
+            };
+        }]
+    };
+});
+
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('searchResult', function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/app/chm/directives/search/search-result.partial.html?'+(new Date().getTime()),
+        controller: ["$scope", function ($scope) {
+
+            $scope.schema      = $scope.document.schema_EN_t.toUpperCase();
+            $scope.title       = $scope.document.title_t;
+            $scope.description = $scope.document.description_t;
+
+            if($scope.document.schema_s=='decision' && $scope.document.body_s=='XXVII8-COP' ) $scope.source = 'COP TO THE CONVENTION';
+            if($scope.document.schema_s=='decision' && $scope.document.body_s=='XXVII8b-MOP') $scope.source = 'COP-MOP TO THE CARTAGENA PROTOCOL ON BIOSAFETY';
+            if($scope.document.schema_s=='decision') $scope.title       = 'Decision ' + $scope.document.code_s;
+            if($scope.document.schema_s=='decision') $scope.description = $scope.document.title_t;
+
+            if($scope.document.schema_s=='recommendation' && $scope.document.body_s=='XXVII8-SBSTTA') { $scope.source = 'SBSTTA'; $scope.sourceTooltip = 'Subsidiary Body on Scientific, Technical and Technological Advice'; }
+            if($scope.document.schema_s=='recommendation' && $scope.document.body_s=='XXVII8-WGRI'  ) { $scope.source = 'WGRI';   $scope.sourceTooltip = 'Working Group on the Review of Implementation'; }
+            if($scope.document.schema_s=='recommendation' && $scope.document.body_s=='XXVII8b-ICCP' ) { $scope.source = 'ICCP';   $scope.sourceTooltip = 'Intergovernmental Committee for the Cartagena Protocol on Biosafety'; }
+            if($scope.document.schema_s=='recommendation' && $scope.document.body_s=='XXVII8c-ICNP' ) { $scope.source = 'ICNP';   $scope.sourceTooltip = 'Intergovernmental Committee for the Nagoya Protocol on ABS'; }
+            if($scope.document.schema_s=='recommendation') $scope.title       = 'Recommendation ' + $scope.document.code_s;
+            if($scope.document.schema_s=='recommendation') $scope.description = $scope.document.title_t;
+
+            if($scope.document.schema_s=='meetingDocument') $scope.source      = $scope.document.meeting_s;
+            if($scope.document.schema_s=='meetingDocument') $scope.title       = $scope.document.symbol_s;
+            if($scope.document.schema_s=='meetingDocument') $scope.description = $scope.document.title_t;
+            if($scope.document.schema_s=='meetingDocument' && $scope.document.group_s=='INF') $scope.source += ' - INFORMATION';
+            if($scope.document.schema_s=='meetingDocument' && $scope.document.group_s=='OFC') $scope.source += ' - PRE-SESSION';
+
+            ////
+
+            $scope.formatDate = function formatDate (date) {
+                return moment(date).format('MMMM Do YYYY');
+            };
+        }]
+    };
+});
+
+//============================================================
+//
+//
+//============================================================
+angular.module('kmApp').compileProvider.directive('toggle', [function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            if(attrs.toggle=='tooltip')
+                element.tooltip();
+        }
+    };
+}]);

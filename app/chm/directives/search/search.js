81eb8e2cd080b30edf3aa48b3054b342764805e8
Philippe Leblond <philippe.leblond@cbd.int>
2013-10-28 23:38:31 -0400

    moved search-filter-keywords JS code under a distinct file (2/2)

--- app/chm/directives/search/search.js
+++ app/chm/directives/search/search.js
@@ -64,8 +64,9 @@ angular.module('kmApp').compileProvider // lazy
             $scope.queryDate     = '*:*';
             $scope.queryKeywords = '*:*';
 
-            if($location.search().q)
+            if($location.search().q) {
                 $scope.keywords = $location.search().q;
+            }
 
             $scope.fixHtml = function (htmlText) {
                 htmlText = (htmlText || "").replace(/\r\n/g, '<br>')
@@ -303,44 +304,6 @@ angular.module('kmApp').compileProvider // lazy
             });
 
             $scope.updateQuery = updateQuery;
-        }]
-    }
-})
-
-//============================================================
-//
-//
-//============================================================
-angular.module('kmApp').compileProvider // lazy
-.directive('searchFilterKeywords', function ($http) {
-    return {
-        restrict: 'EAC',
-        templateUrl: '/app/chm/directives/search/search-filter-keywords.partial.html?'+(new Date().getTime()),
-        replace: true,
-        // require : "?ngModel",
-        scope: {
-            // placeholder: '@',
-            // ngDisabledFn : '&ngDisabled',
-            title: '@title',
-            value: '=ngModel',
-            query: '=query',
-            // locales    : '=',
-            // rows       : '=',
-            // required   : "@"
-        },
-        link: function ($scope, element, attrs, ngModelController) {
-        },
-        controller: ["$scope", function ($scope) {
-
-            $scope.value = $scope.value||'';
-
-            $scope.updateQuery = function () {
-                $scope.query.q = '';
-            };
-
-            $scope.$watch('value', function () {
-                $scope.query = $scope.value == '' ? '*:*' : '(title_t:' + $scope.value + '* OR description_t:' + $scope.value + '* OR text_EN_txt:' + $scope.value + '*)';
-            });
         }]
     }
 })

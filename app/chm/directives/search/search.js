4052be37001b3adb8c392af77051a3756021774f
Philippe Leblond <philippe.leblond@cbd.int>
2013-10-29 00:58:55 -0400

    search result layout for 'National Reports'

--- app/chm/directives/search/search.js
+++ app/chm/directives/search/search.js
@@ -128,7 +128,7 @@ angular.module('kmApp').compileProvider // lazy
                 var queryParameters = {
                     'q': q,
                     'sort': 'createdDate_dt desc, title_t asc',
-                    'fl': 'id,title_t,description_t,url_ss,schema_EN_t,date_dt,government_EN_t,schema_s,number_d,aichiTarget_ss,reference_s,sender_s,meeting_ss,recipient_ss,symbol_s,eventCity_EN_t,eventCountry_EN_t,startDate_s,endDate_s,body_s,code_s,meeting_s,group_s,function_t,department_t,organization_t',
+                    'fl': 'id,title_t,description_t,url_ss,schema_EN_t,date_dt,government_EN_t,schema_s,number_d,aichiTarget_ss,reference_s,sender_s,meeting_ss,recipient_ss,symbol_s,eventCity_EN_t,eventCountry_EN_t,startDate_s,endDate_s,body_s,code_s,meeting_s,group_s,function_t,department_t,organization_t,summary_EN_t,reportType_EN_t',
                     'wt': 'json',
                     'start': $scope.currentPage * $scope.itemsPerPage,
                     'rows': 25,
@@ -476,6 +476,9 @@ angular.module('kmApp').compileProvider.directive('searchResult', function () {
             if($scope.document.schema_s=='meetingDocument') $scope.description = $scope.document.title_t;
             if($scope.document.schema_s=='meetingDocument' && $scope.document.group_s=='INF') $scope.source += ' - INFORMATION';
             if($scope.document.schema_s=='meetingDocument' && $scope.document.group_s=='OFC') $scope.source += ' - PRE-SESSION';
+
+            if($scope.document.schema_s=='nationalReport') $scope.description = $scope.document.summary_EN_t;
+            if($scope.document.schema_s=='nationalReport') $scope.type        = $scope.document.reportType_EN_t;
 
             ////
 

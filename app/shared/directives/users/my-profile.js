fd930b366e5233159ae504a2d9803ccacbd26ee3
edofonsoudbc <eric.dofonsou.consultant@cbd.int>
2013-11-20 11:19:02 -0500

    + Enable save and load of user profile.

--- app/shared/directives/users/my-profile.js
+++ app/shared/directives/users/my-profile.js
@@ -8,6 +8,7 @@ angular.module('kmApp').compileProvider // lazy
         transclude: false,
         scope: false,
         link: function ($scope, $element, $attr, $ctrl) {
+            $scope.init();
         },
         controller: ['$scope' , '$filter', '$location', 'URI', function ($scope, $filter, $location, URI) {
 
@@ -16,38 +17,72 @@ angular.module('kmApp').compileProvider // lazy
                 countries                   : function() { return $http.get("/api/v2013/thesaurus/domains/countries/terms",            { cache: true }).then(function(o){ return $filter('orderBy')(o.data, 'name'); }); },
             };
 
+            //==================================
+            //
+            //==================================
+            $scope.init = function() {
+                $http.get('/api/v2013/users/'+$scope.user.userID).
+                success(function (data, status, headers, config) {
+                    $scope.document = data;
+                    $scope.loadPhones();
+                    $scope.loadFaxes();
+                    $scope.loadEmails();
+                    //debugger;
+                  }).
+                  error(function (data, status, headers, config) {
+                    // ...
+                  });
+            }
+
             //==============================
             //
             //==============================
             $scope.getPhones = function () 
             {
-                if($scope.document.phones==undefined)
+                if($scope.phones==undefined)
                 {
-                    $scope.document.phones = [];
+                    $scope.phones = [];
                 }
 
-                if($scope.document.phones.length==0)
-                    $scope.document.phones.push({value : "", type: ""});
+                if($scope.phones.length==0)
+                    $scope.phones.push({value : "", type: ""});
 
-                var sLastValue = $scope.document.phones[$scope.document.phones.length-1].value; 
-                var sLastType  = $scope.document.phones[$scope.document.phones.length-1].type; 
-                var sLastExt   = $scope.document.phones[$scope.document.phones.length-1].ext; 
+                var sLastValue = $scope.phones[$scope.phones.length-1].value; 
+                var sLastType  = $scope.phones[$scope.phones.length-1].type; 
+                var sLastExt   = $scope.phones[$scope.phones.length-1].ext; 
 
                 //NOTE: IE can set value to 'undefined' for a moment
                 if((sLastValue && sLastValue!="") ||
-                   (sLastType && sLastType!="")   ||
-                   (sLastExt && sLastExt!="") )
-                    $scope.document.phones.push({value : ""});
+                   (sLastType  && sLastType!="")  ||
+                   (sLastExt   && sLastExt!="") )
+                    $scope.phones.push({value : ""});
 
-                return $scope.document.phones;
+                return $scope.phones;
             };
+
+            //==============================
+            //
+            //==============================
+            $scope.loadPhones = function ()
+            {
+                $scope.phones = angular.toJson($scope.document.phones);
+            }
+
+            //==============================
+            //
+            //==============================
+            $scope.savePhones = function ()
+            {
+                $scope.document.phones = angular.fromJson($scope.phones);                
+            }
 
             //==============================
             //
             //==============================
             $scope.removePhone = function(index) 
             {
-                $scope.document.phones.splice(index, 1);
+                $scope.phones.splice(index, 1);
+                savePhones();
             }
 
             //==============================
@@ -55,31 +90,48 @@ angular.module('kmApp').compileProvider // lazy
             //==============================
             $scope.getFaxes = function () 
             {
-                if($scope.document.faxes==undefined)
+                if($scope.faxes==undefined)
                 {
-                    $scope.document.faxes = [];
+                    $scope.faxes = [];
                 }
 
-                if($scope.document.faxes.length==0)
-                    $scope.document.faxes.push({value : "", type: ""});
+                if($scope.faxes.length==0)
+                    $scope.faxes.push({value : "", type: ""});
 
-                var sLastValue = $scope.document.faxes[$scope.document.faxes.length-1].value; 
-                var sLastExt = $scope.document.faxes[$scope.document.faxes.length-1].ext; 
+                var sLastValue = $scope.faxes[$scope.faxes.length-1].value; 
+                var sLastExt = $scope.faxes[$scope.faxes.length-1].ext; 
 
                 //NOTE: IE can set value to 'undefined' for a moment
                 if((sLastValue && sLastValue!="") ||
                    (sLastExt && sLastExt!="") )
-                    $scope.document.faxes.push({value : ""});
+                    $scope.faxes.push({value : ""});
 
-                return $scope.document.faxes;
+                return $scope.faxes;
             };
+
+            //==============================
+            //
+            //==============================
+            $scope.loadFaxes = function ()
+            {
+                $scope.phones = angular.toJson($scope.document.phones);
+            }
+
+            //==============================
+            //
+            //==============================
+            $scope.saveFaxes = function ()
+            {
+                $scope.document.phones = angular.fromJson($scope.phones);                
+            }
 
             //==============================
             //
             //==============================
             $scope.removeFaxe = function(index) 
             {
-                $scope.document.faxes.splice(index, 1);
+                $scope.faxes.splice(index, 1);
+                saveFaxes();
             }
 
             //==============================
@@ -87,30 +139,61 @@ angular.module('kmApp').compileProvider // lazy
             //==============================
             $scope.getEmails = function () 
             {
-                if($scope.document.emails==undefined)
+                if($scope.emails==undefined)
                 {
-                    $scope.document.emails = [];
+                    $scope.emails = [];
                 }
 
-                if($scope.document.emails.length==0)
-                    $scope.document.emails.push({value : ""});
+                if($scope.emails.length==0)
+                    $scope.emails.push({value : ""});
 
-                var sLastValue = $scope.document.emails[$scope.document.emails.length-1].value; 
+                var sLastValue = $scope.emails[$scope.emails.length-1].value; 
 
                 //NOTE: IE can set value to 'undefined' for a moment
                 if(sLastValue && sLastValue!="")
-                    $scope.document.emails.push({value : ""});
+                    $scope.emails.push({value : ""});
 
-                return $scope.document.emails;
+                return $scope.emails;
             };
+
+            //==============================
+            //
+            //==============================
+            $scope.loadEmails = function ()
+            {
+                $scope.phones = angular.toJson($scope.document.phones);
+            }
+
+            //==============================
+            //
+            //==============================
+            $scope.saveEmails = function ()
+            {
+                $scope.document.phones = angular.fromJson($scope.phones);                
+            }
 
             //==============================
             //
             //==============================
             $scope.removeEmail = function(index) 
             {
-                $scope.document.emails.splice(index, 1);
+                $scope.emails.splice(index, 1);
+                saveEmails();
             }
+
+            //==================================
+            //
+            //==================================
+            $scope.onPostSave = function(data) {
+                $http.post('/api/v2013/users/',
+                    angular.toJson($scope.document)).then(
+                    function(resp)
+                    {
+                        return resp.data;
+                    }
+                )
+            };
+
         }]
     }
 }]);

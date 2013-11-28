8e3f85a9c2049dab6ccf5fdbe2c2b11f38324fba
Philippe Leblond <philippe.leblond@cbd.int>
2013-11-28 15:12:18 -0500

    add support for accounts.cbd.int

--- server.js
+++ server.js
@@ -16,14 +16,18 @@ app.configure(function() {
     app.use(express.logger('dev'));
     app.use(express.compress());
     app.use('/app', express.static(__dirname + '/app', { maxAge: oneDay }));
+    app.use('/public', express.static(__dirname + '/public', { maxAge: oneDay }));
     app.use('/favicon.ico', express.static(__dirname + '/favicon.ico', { maxAge: oneDay }));
 });
+
 
 // Configure routes
 
 var proxy = new httpProxy.RoutingProxy();
 
-app.get   ('/app/*', function(req, res) { res.send('404', 404); } );
+app.get   ('/app/*'   , function(req, res) { res.send('404', 404); } );
+app.get   ('/public/*', function(req, res) { res.send('404', 404); } );
+
 app.get   ('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'bch.cbd.int', port: 80 }); } );
 app.put   ('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'bch.cbd.int', port: 80 }); } );
 app.post  ('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeOrigin: true, host: 'bch.cbd.int', port: 80 }); } );
@@ -32,9 +36,17 @@ app.delete('/api/*', function(req, res) { proxy.proxyRequest(req, res, { changeO
 // Configure index.html
 
 app.get('/*', function(req, res) {
-	fs.readFile(__dirname + '/app/index.html', 'utf8', function (error, text) { 
-		res.send(text); 
-	});
+
+	if(req.headers.host=='accounts.cbd.int' || req.headers.host=='localhost:2000') {
+		fs.readFile(__dirname + '/app/accounts.cbd.int.html', 'utf8', function (error, text) { 
+			res.send(text); 
+		});
+	}
+	else {
+		fs.readFile(__dirname + '/app/index.html', 'utf8', function (error, text) { 
+			res.send(text); 
+		});
+	}
 });
 
 // Start server

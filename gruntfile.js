e0dae8d738cc060a603037625f223314b95f8e51
Blaise Fonseca <blaisefonseca@hotmail.com>
2014-05-06 19:26:05 -0400

    branding changes

---
+++ gruntfile.js
@@ -0,0 +1,28 @@
+
+
+module.exports = function(grunt) {
+
+ //Initializing the configuration object
+      grunt.initConfig({
+
+            less: {
+              development: {
+                options: {
+                  paths: ["assets/css"]
+                },
+                files: {
+                  "./app/css/bootstrap.css":"./app/css/SCBDBootstrapVariables.less"
+                }
+              }
+            }
+
+            
+      });
+
+      grunt.loadNpmTasks('grunt-contrib-concat');
+      grunt.loadNpmTasks('grunt-contrib-uglify');
+      grunt.loadNpmTasks('grunt-contrib-less');
+      grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.registerTask('default', ['watch']);
+
+};

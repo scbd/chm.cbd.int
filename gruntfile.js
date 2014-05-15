80146d3e37181e82ebbe9e600f8e3084c58a770c
Blaise Fonseca <blaisefonseca@hotmail.com>
2014-05-15 09:15:18 -0400

    include bower in package.json

--- gruntfile.js
+++ gruntfile.js
@@ -24,5 +24,5 @@ module.exports = function(grunt) {
       grunt.loadNpmTasks('grunt-contrib-less');
       grunt.loadNpmTasks('grunt-contrib-watch');
       grunt.registerTask('default', ['watch']);
-      grunt.registerTask('default', ['bower']);
+      // grunt.registerTask('default', ['bower']);
 };

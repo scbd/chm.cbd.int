36a58fbd33879dd00e3177ea337e75b2bf973023
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-03-24 09:45:28 -0400

    Uses of  load-grunt-tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,15 @@
 /* jshint node:true */
 module.exports = function(grunt) { 'use strict';
 
+    // load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+    require('load-grunt-tasks')(grunt);
+
     // Project configuration.
     grunt.initConfig({
         bower: {
             install: { }//just run 'grunt bower:install' and you'll see files from your Bower packages in lib directory
         }
     });
-    
-    grunt.loadNpmTasks('grunt-bower-task');
 
     grunt.registerTask('default', ['bower']);
 };

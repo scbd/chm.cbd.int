7fee7b4a1834362f4330c0c287ff81d68ef65086
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-05-18 14:07:16 -0400

    facet changes, make variables  local instead of $scope
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/management/select/online-reporting.js
+++ app/views/management/select/online-reporting.js
@@ -15,9 +15,10 @@ define(['app', 'lodash', 'authentication', 'utilities/km-storage', 'utilities/km
                     { identifier: 'implementationActivity'  }
             ];
 
-        var nationalReportTypes = $http.get('/api/v2013/thesaurus/domains/2FD0C77B-D30B-42BC-8049-8C62D898A193/terms').then(function(response) { return response.data; });
-        var cbdNBSAPs          = ["B0EBAE91-9581-4BB2-9C02-52FCF9D82721"];// NBSAP
-        var cbdNationalReports = ["B3079A36-32A3-41E2-BDE0-65E4E3A51601", // NR5
+        //var nationalReportTypes = $http.get('/api/v2013/thesaurus/domains/2FD0C77B-D30B-42BC-8049-8C62D898A193/terms').then(function(response) { return response.data; });
+        var cbdNBSAPs          = [   "B0EBAE91-9581-4BB2-9C02-52FCF9D82721"];// NBSAP
+
+        var cbdNationalReports = [   "B3079A36-32A3-41E2-BDE0-65E4E3A51601", // NR5
                                      "272B0A17-5569-429D-ADF5-2A55C588F7A7", // NR4
                                      "DA7E04F1-D2EA-491E-9503-F7923B1FD7D4", // NR3
                                      "A49393CA-2950-4EFD-8BCC-33266D69232F", // NR2
@@ -55,7 +56,7 @@ define(['app', 'lodash', 'authentication', 'utilities/km-storage', 'utilities/km
                 // All 3 National Reports
                 ////////////////
                 var publisehdReportQuery = $http.get('/api/v2013/index/select?facet=true&facet.limit=512&facet.pivot=schema_s,reportType_s&fl=&facet.mincount=1&q=(realm_ss:chm AND schema_s:nationalReport '+
-                    ')+AND+NOT+version_s:*&rows=0&wt=json');
+                    ')+AND+NOT+version_s:*+AND+NOT+workflow_s:*&rows=0&wt=json');
                 var draftReportQuery = $http.get('/api/v2013/index/select?facet=true&facet.limit=512&facet.pivot=schema_s,reportType_s&fl=&facet.mincount=1&q=(realm_ss:chm AND schema_s:nationalReport '+
                     ')+AND+version_s:*+AND+NOT+workflow_s:*&rows=0&wt=json');
                 var requestReportQuery = $http.get('/api/v2013/index/select?facet=true&facet.limit=512&facet.pivot=schema_s,reportType_s&fl=&facet.mincount=1&q=(realm_ss:chm AND schema_s:nationalReport '+
@@ -136,7 +137,8 @@ define(['app', 'lodash', 'authentication', 'utilities/km-storage', 'utilities/km
             var qqNationalReports = _.filter(list, function(o) { return   _.contains(cbdNationalReports, o.value); });
             var qqNBSAPs          = _.filter(list, function(o) { return   _.contains(cbdNBSAPs,          o.value); });
 
-            var others= cbdNationalReports.concat($scope.cbdNBSAPs);
+            var others= cbdNationalReports.concat(cbdNBSAPs);
+
             var qqOthers          = _.filter(list, function(o) { return  !_.contains(others, o.value); });
 
             //summmation

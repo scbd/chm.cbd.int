a546a49ce8b94f09b20ebf6df0e4dcca68495ac3
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-06-11 15:12:28 -0400

    Change deploy server

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-docker build -t registry.infra.cbd.int:5000/chm-cbd-int git@github.com:scbd/chm.cbd.int.git
-docker push     registry.infra.cbd.int:5000/chm-cbd-int
+docker build -t localhost:5000/chm-cbd-int git@github.com:scbd/chm.cbd.int.git
+docker push     localhost:5000/chm-cbd-int

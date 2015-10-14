4644efd0a62a664db751be9821cc8cd0b436225f
unknown <randy.houlahan@Houlahan.biodiv.org>
2015-10-14 10:57:44 -0400

    add parent directive functionality, remove unused code

--- app/directives/search/search-filter-keywords.js
+++ app/directives/search/search-filter-keywords.js
@@ -6,41 +6,36 @@ define(['text!./search-filter-keywords.html', 'app'], function(template, app) { 
         restrict: 'EAC',
         template: template,
         replace: true,
-        // require : "?ngModel",
+        require : '^search',
         scope: {
-            // placeholder: '@',
-            // ngDisabledFn : '&ngDisabled',
+
             title: '@title',
             value: '=ngModel',
             query: '=query',
-            // locales    : '=',
-            // rows       : '=',
-            // required   : "@"
+
         },
-        link: function ($scope) {
-
+      link : function ($scope, $element, $attr, searchCtrl)//jshint ignore:line
+      {
             applyKeywords($scope.value || $location.search().keywords || '');
-
-            $scope.updateQuery = function () {
-                $scope.query.q = '';
-            };
 
             $scope.$watch('value', function (keywords) {
 
                 $location.replace();
                 $location.search("keywords", keywords || null);
-
                 applyKeywords(keywords);
             });
 
             function applyKeywords(keywords)
             {
                 $scope.value = keywords;
-
-                $scope.query = !keywords ? '*:*' : '(title_t:"' + keywords + '*" OR description_t:"' + keywords + '*" OR text_EN_txt:"' + keywords + '*")';
+                if(keywords)
+                    searchCtrl.addSubQuery('keywords','(title_t:"' + keywords + '*" OR description_t:"' + keywords + '*" OR text_EN_txt:"' + keywords + '*")',true);
+                else
+                    searchCtrl.deleteAllSubQuery('keywords');
+                searchCtrl.search();
             }
-        }
-    };
-}]);
 
-});
+      }//link
+    }; // return
+  }]);  //app.directive('
+});// define

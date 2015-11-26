8b4d21b6d965506813ecea5947a8244cc6a1f332
Randy Houlahan <randy.houlahan@cbd.int>
2015-11-26 18:44:48 -0500

    test page contorller for amman

---
+++ app/views/search/reporting-map.js
@@ -0,0 +1,12 @@
+define(["lodash", 'directives/ammap3'], function(_) { 'use strict';
+
+    return ["$scope", "$http", "realm",
+     function ($scope, $http, realm) {
+
+        $scope.countries=[];
+        $scope.colors={};
+
+
+
+    }];
+});

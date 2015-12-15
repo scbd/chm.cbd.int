d63adfb470dc37d3378a2dff4f29f4f3e0c6ba78
Randy Houlahan <randy.houlahan@cbd.int>
2015-12-14 20:16:53 -0500

    Ad in national indicator filter directive

---
+++ app/directives/reporting-display/filter-indicator.js
@@ -0,0 +1,29 @@
+define(['app','text!./filter-indicator.html'], function(app,template) {
+  'use strict';
+  app.directive('filterIndicator', [function() {
+    return {
+      restrict: 'EAC',
+      template: template,
+      replace: true,
+      require: '^reportingDisplay',
+      scope: {
+        title: '@title',
+        items: '=ngModel',
+        facet: '@facet',
+        count: '=count' // total count of all children subquires needed for 0 result combinations
+      },
+      link: function($scope, $element, $attr, reportingDisplayCtrl) {
+
+          //=======================================================================
+          //
+          //=======================================================================
+          $scope.loadRecords = function() {
+
+            reportingDisplayCtrl.addSubQuery('nationalIndicator', 'schema_s:nationalIndicator  AND _latest_s:true AND _state_s:public ', true);
+            reportingDisplayCtrl.search();
+          }; // flatten
+
+        } //link
+    }; // return
+  }]); //app.directive('searchFilterCountries
+}); // define

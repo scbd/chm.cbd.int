78eb7f5a4671cd5ceada972e0268bdf39ccad337
Randy Houlahan <randy.houlahan@cbd.int>
2015-12-30 13:17:11 -0500

    Security update for stats page

--- app/views/management/stats/resource-mobilisation.js
+++ app/views/management/stats/resource-mobilisation.js
@@ -1,11 +1,12 @@
 define(['app', "directives/stats-display/amchart3","directives/stats-display/results-list",'authentication'], function() { 'use strict'; //"directives/stats-display/stats-display",
 
-    return ['$scope',  '$http', '$timeout', '$filter',
-     function($scope,  $http, $timeout, $filter ) {
+    return ['$scope',  '$http', '$timeout', '$filter','$rootScope',
+     function($scope,  $http, $timeout, $filter,$rootScope ) {
 
         $scope.schema="resourcemobilization";
         $scope.chartData=[];
-
+        $scope.government = userGovernment();
+        
         $http.get("/api/v2013/thesaurus/domains/countries/terms", {cache: true}).then(function(o) {
             $scope.countries = $filter('orderBy')(o.data, 'title');
           return;
@@ -22,6 +23,14 @@ define(['app', "directives/stats-display/amchart3","directives/stats-display/res
             $scope.documents= o.data;
           return;
         });
-
+        //==============================
+        //
+        //==============================
+        function userGovernment() {
+            if($rootScope.user.government){
+                $scope.showNational = true;
+                return $rootScope.user.government.toLowerCase();
+            }
+        }
     }];
 });

a64ecee0a1974efa528e33049368d25130a13c0c
Randy Houlahan <randy.houlahan@cbd.int>
2016-01-07 19:27:17 -0500

    change to IE 10

--- spec/conf.js
+++ spec/conf.js
@@ -19,7 +19,7 @@ exports.config = {
   }, {
     browserName: 'internet explorer',
     platform: 'Windows 7',
-    version: '9.0'
+    version: '10.0'
   }, {
     browserName: 'safari',
     platform: 'OS X 10.10',

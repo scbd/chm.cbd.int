3a3785441569abb63594b3074412f0645d4aa2de
Randy Houlahan <randy.houlahan@cbd.int>
2016-01-07 20:33:16 -0500

    clean up

--- spec/reportsMapSpec.js
+++ spec/reportsMapSpec.js
@@ -1,26 +1,14 @@
 //To get sjhint to ignore protrator calls ad this to .jshint "browser","element","by","expect","protractor","beforeEach"
-describe('Sign-In Page', function() { //jshint ignore:line
+describe('Online Reporting Search Page', function() { //jshint ignore:line
 
   var EC = protractor.ExpectedConditions;
 
   beforeEach(function() {
     browser.get('/search/reporting-map?filter=all');
-    browser.driver.sleep(3);
+    browser.driver.sleep(1);
     browser.waitForAngular();
     //       spyOn(console, 'error');
   }); // it
-
-  // it('should have a title', function() { //jshint ignore:line
-  //   expect(browser.getTitle()).toEqual('Accounts: Convention on Biological Diversity');
-  // }); // it
-
-  // it('should show alert message when invalid email/password is submitted', function() { //jshint ignore:line
-  //   element(by.model('email')).sendKeys(1);
-  //   element(by.model('password')).sendKeys(2);
-  //   $('.btn-primary').click().then(function() {
-  //     EC.visibilityOf($('#failedLoginAlert'));
-  //   });
-  // }); // it
 
   it('should not report errors when the page is loaded', function() {
 

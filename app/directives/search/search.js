d728cbf6fb9199f1d12f569413e30744e46d9102
Randy Houlahan <randy.houlahan@cbd.int>
2016-06-27 09:03:39 -0400

    filter to convert object to array, ng-repeat not liking object for orderBy and filter anymore.

--- app/directives/search/search.js
+++ app/directives/search/search.js
@@ -447,4 +447,23 @@ define(['text!./search.html',
 	        }
 	    };
 	}]); //directive
+
+	app.filter('toArray', function () {
+	  return function (obj, addKey) {
+	    if (!_.isObject(obj)) return obj;
+	    if ( addKey === false ) {
+	      return Object.keys(obj).map(function(key) {
+	        return obj[key];
+	      });
+	    } else {
+	      return Object.keys(obj).map(function (key) {
+	        var value = obj[key];
+	        return _.isObject(value) ?
+	          Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+	          { $key: key, $value: value };
+	      });
+	    }
+	  };
+	});
+
 });//define

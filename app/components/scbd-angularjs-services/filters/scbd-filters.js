3cd5ef9ddf3ae57d6cd8cf61046828a357e43915
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-02-13 12:51:31 -0500

    handle notification url

--- app/components/scbd-angularjs-services/filters/scbd-filters.js
+++ app/components/scbd-angularjs-services/filters/scbd-filters.js
@@ -267,50 +267,13 @@ define(['app', 'moment'], function (app, moment) {
       return sText || "";
     };
   });
-
-  //============================================================
-  //
-  //
-  //
-  //============================================================
-  // app.filter("schemaName", [function() {
-	// 	return function( schema ) {
-	// 		if(!schema)return schema;
-	// 		if(schema.toLowerCase()=="focalpoint"				            ) return "ABS National Focal Point";
-	// 		if(schema.toLowerCase()=="authority"				            ) return "Competent National Authority";
-	// 		if(schema.toLowerCase()=="contact"					            ) return "Contact";
-	// 		if(schema.toLowerCase()=="database"					            ) return "National Website or Database";
-	// 		if(schema.toLowerCase()=="resource"					            ) return "Virtual Library Resource";
-	// 		if(schema.toLowerCase()=="organization"				          ) return "Organization";
-	// 		if(schema.toLowerCase()=="measure" 					            ) return "Legislative, Administrative or Policy Measure";
-	// 		if(schema.toLowerCase()=="abscheckpoint"			          ) return "Checkpoint";
-	// 		if(schema.toLowerCase()=="abscheckpointcommunique"	    ) return "Checkpoint Communiqué";
-	// 		if(schema.toLowerCase()=="abspermit"				            ) return "Internationally Recognized Certificate of Compliance";
-  //     if(schema.toLowerCase()=="meetingdocument"			        ) return "Meeting Document";
-  //     if(schema.toLowerCase()=="pressrelease"				          ) return "Press Release";
-	// 		if(schema.toLowerCase()=="news"						              ) return "News";
-	// 		if(schema.toLowerCase()=="new"						              ) return "What's New";
-  //     if(schema.toLowerCase()=="statement"			              ) return "Statement";
-	// 		if(schema.toLowerCase()=="absnationalreport"		        ) return "Interim National Report on the Implementation of the Nagoya Protocol";
-	// 		if(schema.toLowerCase()=="modelcontractualclause"	      ) return "Model Contractual Clauses, Codes of Conduct, Guidelines, Best Practices and/or Standard";
-	// 		if(schema.toLowerCase()=="communityprotocol"		        ) return "Community Protocol and Procedures and Customary Law";
-	// 		if(schema.toLowerCase()=="meeting"					            ) return "Meeting";
-	// 		if(schema.toLowerCase()=="notification"				          ) return "Notification";
-	// 		if(schema.toLowerCase()=="capacitybuildinginitiative"   ) return "Capacity-building Initiative";
-	// 		if(schema.toLowerCase()=="capacitybuildingresource"     ) return "Capacity-building Resource";
-	// 		if(schema.toLowerCase()=="endorsement"				          ) return "Endorsement";
-
-  //     return schema;
-
-	// 	};
-	// }]);
-
-  //============================================================
+  
+   //============================================================
 	//
 	//
 	//
 	//============================================================
-	app.filter("schemaShortName", [function() {
+	app.filter("urlSchemaShortName", [function() {
 
 		return function( schema ) {
 
@@ -340,6 +303,8 @@ define(['app', 'moment'], function (app, moment) {
 			if(schema.toLowerCase()=="capacitybuildinginitiative" ) return "CBI";
 			if(schema.toLowerCase()=="capacitybuildingresource"   ) return "CBR";
 			if(schema.toLowerCase()=="endorsement"				        ) return "EDR";
+
+      return schema;
 		};
 	}]);
 

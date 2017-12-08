915cde1ce55fa9d161ef4a8f840e88764ad68009
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-12-08 15:57:00 -0500

    add document history for admins

---
+++ app/directives/formats/views/directives/document-date.js
@@ -0,0 +1,23 @@
+﻿define(['app', 'text!./document-date.html', 'moment'], function (app, template, moment) {
+    app.directive('documentDate', function () {
+        return {
+            restrict: 'E',
+            template: template,
+            replace: true,
+            link: function ($scope) {
+                
+                 if(($scope.document||{}).createdDate_dt ||
+                    ($scope.$parent.internalDocumentInfo && $scope.$parent.internalDocumentInfo.documentID !== undefined  &&  $scope.$parent.internalDocumentInfo)){
+                    
+                        $scope.updatedOn = ($scope.document||{}).createdDate_dt || $scope.$parent.internalDocumentInfo.updatedOn;
+                        if($scope.$parent.internalDocumentInfo.type=='nationalReport6'){
+                            var createdOn = $scope.$parent.internalDocumentInfo.createdOn;
+                            if(createdOn!=$scope.updatedOn && !moment.utc(createdOn).isSame(moment.utc($scope.updatedOn), 'day')){
+                                $scope.createdOn = createdOn;
+                            }
+                        }
+                }
+            }
+        }
+    })
+})

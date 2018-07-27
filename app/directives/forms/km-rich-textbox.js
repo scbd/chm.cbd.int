cb004dee12ac7e51ce2ed20cfd561cf42090e10c
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-07-27 17:05:07 -0400

    switch to wysiwg for multiline fields

--- app/directives/forms/km-rich-textbox.js
+++ app/directives/forms/km-rich-textbox.js
@@ -6,7 +6,7 @@ function(app,template,angular) {
 	//
 	//
 	//============================================================
-	app.directive('kmRichTextbox', function() {
+	app.directive('kmRichTextbox', ["IStorage", function(storage) {
 			return {
 					restrict: 'EAC',
 					template: template,
@@ -25,126 +25,147 @@ function(app,template,angular) {
 							onFileUpload : '&?'
 					},
 					link: function($scope, element, attrs, ngModelController) {
-							$scope.text = {};
-							$scope.$watch('locales', $scope.watchLocales);
-							$scope.$watch('binding', $scope.watchBinding);
-							$scope.$watch('binding', function() {
-									try {
-											ngModelController.$setViewValue($scope.binding);
-									} catch (e) {}
+						
+						$scope.maxFileSize = attrs.maxFileSize || 1000000;
+						$scope.editorHeightClass = attrs.editorHeightClass||'ta-editor-height-100';	
+
+						$scope.isUploadingImage = {};
+						var activeLocale;
+						if(!$scope.toolbar)
+							$scope.toolbar = "[['bold','italics', 'underline'],['ul', 'ol', 'undo', 'redo', 'clear'],['wordcount', 'charcount'], ['insertImage']]";
+								// ['justifyLeft','justifyCenter','justifyRight','justifyFull','indent','outdent'],						
+
+						//==============================
+						//
+						//==============================
+						$scope.$watch('binding', function(text) {
+
+							text = text || {};
+
+							_.forEach($scope.locales || _.keys(text), function(l){
+								text[l] = text[l] || "";
 							});
 
-							$scope.maxFileSize = attrs.maxFileSize || 1000000;
-							$scope.editorHeightClass = attrs.editorHeightClass||'ta-editor-height-100';
-					},
-					controller: ["$scope", "IStorage", function($scope, storage) {
-							$scope.isUploadingImage = {};
-							var activeLocale;
-							if(!$scope.toolbar)
-								$scope.toolbar = "[['bold','italics', 'underline'],['ul', 'ol', 'undo', 'redo', 'clear'],['wordcount', 'charcount'], ['insertImage']]";
-									// ['justifyLeft','justifyCenter','justifyRight','justifyFull','indent','outdent'],
-							//==============================
-							//Remove value of not selected languages/empty languages
-							//==============================
-							$scope.watchLocales = function() {
-									var oLocales = $scope.locales || [];
-									var oBinding = $scope.binding || {};
-									var oText = $scope.text;
+							$scope.text = _.defaults(text, $scope.text||{});
 
-									angular.forEach(oLocales, function(locale, i) {
-											oText[locale] = oBinding[locale] || oText[locale];
-									});
-							};
+							updateText();
 
-							//==============================
-							//Remove value of not selected languages/empty languages
-							//==============================
-							$scope.watchBinding = function() {
-									var oLocales = $scope.locales || [];
-									var oBinding = $scope.binding || {};
-									var oText = $scope.text;
+						}, true);
 
-									angular.forEach(oLocales, function(locale, i) {
-											oText[locale] = oBinding[locale];
-									});
-							};
+						//==============================
+						//
+						//==============================
+						$scope.$watch('text',    updateText, true);
+						$scope.$watch('locales', updateText, true);
 
-							//==============================
-							//Remove value of not selected languages/empty languages
-							//==============================
-							$scope.onchange = function() {
-									var oLocales = $scope.locales || [];
-									var oText = $scope.text || {};
-									var oNewBinding = {};
+						//==============================
+						//
+						//==============================
+						function updateText(){
 
-									angular.forEach(oLocales, function(locale, i) {
-											if ($("<i>").html(oText[locale]).text().trim() !== "")
-													oNewBinding[locale] = oText[locale];
-									});
+							var text = _(angular.extend($scope.binding || {}, $scope.text || {})).pick($scope.locales||[]).forEach(function(value, key, text){
+								if(!value || $("<i>").html(text[key]).text().trim() == "")
+									delete text[key];
+							}).value();
 
-									$scope.binding = !$.isEmptyObject(oNewBinding) ? oNewBinding : undefined;
-									$scope.ngChange();
-							};
+							if(_.isEmpty(text))
+								text = undefined;
 
-							//==============================
-							//
-							//==============================
-							$scope.isRequired = function() {
-									return $scope.required !== undefined && $.isEmptyObject($scope.binding);
-							};
+							try { ngModelController.$setViewValue(text); } catch(e) {}
+						}
 
-							//==============================
-							//
-							//==============================
-							$scope.isShowLocale = function() {
-									return $scope.locales && $scope.locales.length > 1;
-							};	
+						//==============================
+						//
+						//==============================
+						$scope.isRequired = function()
+						{
+							return $scope.required!=undefined && $.isEmptyObject($scope.binding); // jshint ignore:line
+						};
 
-							$scope.onFocus = function(locale) {
-								activeLocale = locale;
-							}
+						//==============================
+						//
+						//==============================
+						$scope.isShowLocale = function()
+						{
+							return $scope.locales && $scope.locales.length>1;
+						};
 
-							$scope.onFileDrop = function( file, insertAction, a, b ) {
-								var fileType = file.type.substring( 0, 5 );
-								// if( file.type.substring( 0, 5 ) !== "image" ) {
-								// 	alert( "only images can be added" );
-								// 	return true;
-								// }
-								if(fileType == "image" && file.size > $scope.maxFileSize ) {
-									alert( "file size cannot exceed " + bytesToSize($scope.maxFileSize));
-									return true;
-								}
-								var acLocale = angular.copy(activeLocale);
-								$scope.isUploadingImage[acLocale] = true;
-								storage.attachments.put($scope.identifier, file)
-								.then(function(data){
-									var action = "insertImage"
-									var actionData = data.url;
+						//==============================
+						//Remove value of not selected languages/empty languages
+						//==============================
+						$scope.onchange = function() {
+								var oLocales = $scope.locales || [];
+								var oText = $scope.text || {};
+								var oNewBinding = {};
 
-									if(fileType !== 'image'){
-										action = 'inserthtml'
-										actionData = '<a target="_blank" href="'+ data.url+'">' + data.filename+ '</a>'
-									}
-									insertAction(action , actionData, true );
-									if($scope.onFileUpload && typeof $scope.onFileUpload == 'function'){
-										data.locale =  acLocale;
-										$scope.onFileUpload({data:data});
-									}
-								})
-								.finally(function(){
-									$scope.isUploadingImage[acLocale] = false;
+								angular.forEach(oLocales, function(locale, i) {
+										if ($("<i>").html(oText[locale]).text().trim() !== "")
+												oNewBinding[locale] = oText[locale];
 								});
 
-								return true;
-							};
+								$scope.binding = !$.isEmptyObject(oNewBinding) ? oNewBinding : undefined;
+								$scope.ngChange();
+						};							
 
-							function bytesToSize(bytes) {
-								var sizes = ['Bytes', 'KB', 'MB'];
-								if (bytes == 0) return '0 Byte';
-								var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-								return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-							 };
+						$scope.onFocus = function(locale) {
+							activeLocale = locale;
+						}
+
+						$scope.onFileDrop = function( file, insertAction, a, b ) {
+
+							if(!$scope.identifier){
+								alert("Associated document identifier not set, cannot upload file");
+								return true;
+							}
+
+							var fileType = file.type.substring( 0, 5 );
+							var mimeType = storage.attachments.getMimeType(file);
+							// if( file.type.substring( 0, 5 ) !== "image" ) {
+							// 	alert( "only images can be added" );
+							// 	return true;
+							// }
+							if (storage.attachments.mimeTypeWhitelist.indexOf(mimeType) < 0) {
+								alert("File type not supported: " + mimeType);
+								return true;
+							}
+							if(fileType == "image" && file.size > $scope.maxFileSize ) {
+								alert( "file size cannot exceed " + bytesToSize($scope.maxFileSize));
+								return true;
+							}
+							var acLocale = angular.copy(activeLocale);
+							$scope.isUploadingImage[acLocale] = true;
+							storage.attachments.put($scope.identifier, file)
+							.then(function(data){
+								var action = "insertImage"
+								var actionData = data.url;
+
+								if(fileType !== 'image'){
+									action = 'inserthtml'
+									actionData = '<a target="_blank" href="'+data.url+'">'+data.filename+ '</a>'
+								}
+								insertAction(action , actionData, true );
+								if($scope.onFileUpload && typeof $scope.onFileUpload == 'function'){
+									data.locale =  acLocale;
+									$scope.onFileUpload({data:data});
+								}
+							})
+							.finally(function(){
+								$scope.isUploadingImage[acLocale] = false;
+							});
+
+							return true;
+						};
+
+						function bytesToSize(bytes) {
+							var sizes = ['Bytes', 'KB', 'MB'];
+							if (bytes == 0) return '0 Byte';
+							var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+							return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+							};
+					},
+					controller: ["$scope", "IStorage", function($scope, storage) {
+							
 					}]
 			};
-	});
+	}]);
 });

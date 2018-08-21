16e1d05c229f0f6ea820020926b565d966090778
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-08-21 12:14:01 -0400

    rich text box changes

--- app/app.js
+++ app/app.js
@@ -52,7 +52,10 @@ define(['angular','rangy-core', 'ngSanitize', 'rangy-selectionsave', 'textAngula
                     var editor =  this.$editor();
                     var uploadPlaceholder = $('input[name="' + editor._name + '"]').parent();                    
                     var helpButton = uploadPlaceholder.find('button[name="editorHelp"]');
-                    helpButton.attr('data-content', 'Use the image button to upload images.');
+                    helpButton.attr('data-content', 
+                    'Use the image button to upload images.\n Allowed html elements are img,h1, h2, h3, h4, h5, strong,b, i, '+
+                    'u, blockquote, em, br, p, div,span, ul, ol, li, a, div, span, table, tbody, th, tr, td \n'+
+                    'All other elements will be automatically removed when the record is saved');
                     helpButton.attr('data-title', 'CHM Help: How to use the rich text editor');
                     helpButton.webuiPopover({
                         trigger:'sticky',

bacd84aa9a44b2d036d023e18067677713e0dd4c
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-09-05 23:07:59 -0400

    split tabs into directive and load on demand

---
+++ app/directives/formats/forms/nr6-directives/gspcContribution.js
@@ -0,0 +1,13 @@
+﻿define(['text!./gspcContribution.html', 'app'], function(template, app) { 'use strict';
+
+app.directive("nr6GspcContributionTab", [function () {
+    return {
+        restrict: 'E',
+        template : template,
+        replace: true,
+        transclude: false,
+        link: function ($scope) {
+        }
+    };
+}]);
+});

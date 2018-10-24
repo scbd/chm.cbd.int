887e06ca3204f8244d3e66e3ed8363d7a946f2c8
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-10-24 12:27:15 -0400

    handle images on rte in better way

--- app/directives/forms/km-rich-textbox-custom-controls.js
+++ app/directives/forms/km-rich-textbox-custom-controls.js
@@ -10,26 +10,19 @@ define(['app','rangy-core', 'rangy-selectionsave', 'textAngular'], function(app,
                 iconclass: "fa fa-photo",
                 tooltiptext: 'upload image',
                 action: function(){
-
-                    var editor =  this.$editor();
-                    var uploadPlaceholder = $('input[name="' + editor._name + '"]').parent();                    
-					uploadPlaceholder.children('input[type=file]').remove();
-					uploadPlaceholder.prepend("<input type='file' style='display:none' />");
-
-					var qHtmlInputFile = uploadPlaceholder.children("input[type=file]:first");
-
-					qHtmlInputFile.change(function()
-					{
-                        var file = this.files[0];
-                        
-                        editor.fileDropHandler(file, editor.wrapSelection);
-                        return;
-					});
-
-					qHtmlInputFile.click();
+                    uploadFile(this.$editor(), ".jpg, .jpeg, .bmp, .gif, .png");
                 }
             });
             taOptions.toolbar[1].push('uploadCustomImage');
+
+            taRegisterTool('uploadCustomFile', {
+                iconclass: "fa fa-files-o",
+                tooltiptext: 'upload file',
+                action: function(){
+                    uploadFile(this.$editor());
+                }
+            });
+            taOptions.toolbar[1].push('uploadCustomFile');
 
             taRegisterTool('editorHelp', {
                 disabled:function(){return false},
@@ -40,7 +33,7 @@ define(['app','rangy-core', 'rangy-selectionsave', 'textAngular'], function(app,
                     var uploadPlaceholder = $('input[name="' + editor._name + '"]').parent();                    
                     var helpButton = uploadPlaceholder.find('button[name="editorHelp"]');
                     helpButton.attr('data-content', 
-                    'Use the image button to upload images.\n Allowed html elements are img,h1, h2, h3, h4, h5, strong,b, i, '+
+                    'Use the image button to upload images.\n Use the file button to upload files.\n Allowed html elements are img,h1, h2, h3, h4, h5, strong,b, i, '+
                     'u, blockquote, em, br, p, div,span, ul, ol, li, a, div, span, table, tbody, th, tr, td \n'+
                     'All other elements will be automatically removed when the record is saved');
                     helpButton.attr('data-title', 'CHM Help: How to use the rich text editor');
@@ -65,6 +58,25 @@ define(['app','rangy-core', 'rangy-selectionsave', 'textAngular'], function(app,
                 }
             });
             taOptions.toolbar[1].push('editorHelp');
+
+            function uploadFile(editor, supportedTypes){
+
+                var uploadPlaceholder = $('input[name="' + editor._name + '"]').parent();                    
+                uploadPlaceholder.children('input[type=file]').remove();
+                uploadPlaceholder.prepend("<input type='file' style='display:none'" + (supportedTypes ? " accept='"+supportedTypes+"' " : "") + "/>");
+
+                var qHtmlInputFile = uploadPlaceholder.children("input[type=file]:first");
+
+                qHtmlInputFile.change(function()
+                {
+                    var file = this.files[0];
+                    
+                    editor.fileDropHandler(file, editor.wrapSelection);
+                    return;
+                });
+
+                qHtmlInputFile.click();
+            }
 
             return taOptions;
         }]);
